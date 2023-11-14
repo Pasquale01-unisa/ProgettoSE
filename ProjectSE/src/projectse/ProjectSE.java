fd1977f12c5f312da889eed15293bdb8b3dcafc1
Pasquale01-unisa <129376778+Pasquale01-unisa@users.noreply.github.com>
2023-11-14 15:40:24 +0100

    Prova

--- ProjectSE/src/projectse/ProjectSE.java
+++ ProjectSE/src/projectse/ProjectSE.java
@@ -26,7 +26,7 @@ public class ProjectSE extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Hello Worl!");
             }
         });
         
