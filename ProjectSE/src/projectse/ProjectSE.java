0bddc89d9c1d7b067ca3de4da50d3be4575d4c65
SaraAbagnale <134072819+SaraAbagnale@users.noreply.github.com>
2023-11-14 15:37:44 +0100

    Prova

--- ProjectSE/src/projectse/ProjectSE.java
+++ ProjectSE/src/projectse/ProjectSE.java
@@ -21,7 +21,7 @@ public class ProjectSE extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello Worl'");
+        btn.setText("Say");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
