108ed16514455fd42ad964e01f8137e3bc812c73
SaraAbagnale <134072819+SaraAbagnale@users.noreply.github.com>
2023-11-22 11:28:34 +0100

    Revert "Implementazione campi Alarm"

--- ProjectSE/src/projectse/controller/MyProjectSEViewController.java
+++ ProjectSE/src/projectse/controller/MyProjectSEViewController.java
@@ -127,11 +127,7 @@ public class MyProjectSEViewController implements Initializable {
     
     @FXML
     private void onBtnAlarm(ActionEvent event) {
-        textAction.setDisable(true);
-        btnFile.setManaged(true);
-        btnFile.setVisible(true);
-        textAction.clear();
-        btnAction.setText("Alarm");
+        
     }
     
     @FXML
