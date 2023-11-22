ed56b52053189eefebb3cb4bb82876cb032df8f9
Pasquale01-unisa <129376778+Pasquale01-unisa@users.noreply.github.com>
2023-11-22 10:53:12 +0100

    Creazione package MVC

--- ProjectSE/src/projectse/controller/MyProjectSEViewController.java
+++ ProjectSE/src/projectse/controller/MyProjectSEViewController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package projectse;
+package projectse.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -79,7 +79,7 @@ public class MyProjectSEViewController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-      
+      btnCommit.setDisable(true);
 
     }
 
--- ProjectSE/src/projectse/model/ProjectSE.java
+++ ProjectSE/src/projectse/model/ProjectSE.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
  */
-package projectse;
+package projectse.model;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
@@ -23,7 +23,7 @@ public class ProjectSE extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MyProjectSEViewController.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/projectse/view/MyProjectSEViewController.fxml"));
         primaryStage.setTitle("MyIFTTT");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
