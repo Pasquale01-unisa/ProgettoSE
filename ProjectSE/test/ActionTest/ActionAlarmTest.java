06e9b7260c5a16ed06e1b325b5b0c84b23badf9a
SaraAbagnale <134072819+SaraAbagnale@users.noreply.github.com>
2023-11-23 18:07:45 +0100

    Implementazione classe di test Single Rule

--- ProjectSE/test/ActionTest/ActionAlarmTest.java
+++ ProjectSE/test/ActionTest/ActionAlarmTest.java
@@ -1,15 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
  */
 package ActionTest;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -20,19 +20,19 @@ public class ActionAlarmTest {
     public ActionAlarmTest() {
     }
     
-    @BeforeAll
+    @BeforeClass
     public static void setUpClass() {
     }
     
-    @AfterAll
+    @AfterClass
     public static void tearDownClass() {
     }
     
-    @BeforeEach
+    @Before
     public void setUp() {
     }
     
-    @AfterEach
+    @After
     public void tearDown() {
     }
 
--- ProjectSE/test/RuleTest/SingleRuleTest.java
+++ ProjectSE/test/RuleTest/SingleRuleTest.java
@@ -4,16 +4,15 @@
  */
 package RuleTest;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import projectse.model.action.Action;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertThrows;
+import org.junit.Before;
+import org.junit.Test;
 import projectse.model.action.ActionMemo;
+import projectse.model.rule.Rule;
 import projectse.model.rule.SingleRule;
-import projectse.model.trigger.Trigger;
 import projectse.model.trigger.TriggerTime;
 
 /**
@@ -21,5 +20,67 @@ import projectse.model.trigger.TriggerTime;
  * @author sara
  */
 public class SingleRuleTest {
+    SingleRule rule;
+    SingleRule ruleException;
     
+    @Before
+    public void setUp(){
+        rule = new SingleRule("TestRule", new TriggerTime("00", "00"), new ActionMemo("TestAction"), "Active");
+        ruleException = new SingleRule("TestRule", new TriggerTime("00", "01"), new ActionMemo("TestAction"), "Deactivated");
+    }
+    
+    @Test 
+    public void getSetNameTest(){
+        assertEquals("TestRule", rule.getName());
+
+        rule.setName("NewTestRule");
+        assertEquals("NewTestRule", rule.getName());
+    }
+    
+    @Test 
+    public void getSetTriggerTest(){
+        assertEquals("Time -> 00:00", rule.getTrigger());
+
+        rule.setTrigger(new TriggerTime("01", "00"));
+        assertEquals("Time -> 01:00", rule.getTrigger());
+    }
+    
+    @Test 
+    public void getSetActionTest(){
+        assertEquals("Memo -> TestAction", rule.getAction());
+
+        rule.setAction(new ActionMemo("TestAction2"));
+        assertEquals("Memo -> TestAction2", rule.getAction());
+    }
+    
+    @Test 
+    public void getSetStateTest(){
+        assertEquals("Active", rule.getState());
+
+        rule.setState("Deactivated");
+        assertEquals("Deactivated", rule.getState());
+    }
+    
+    @Test
+    public void getSetIsSelectedTest(){
+        assertFalse(rule.getIsSelected());
+
+        rule.setIsSelected(true);
+        assertTrue(rule.getIsSelected());
+    }
+    
+    @Test(expected=UnsupportedOperationException.class)
+    public void addRuleTest(){
+        rule.addRule(ruleException);
+    }
+   
+    @Test(expected=UnsupportedOperationException.class)
+    public void deleteRuleTest(){
+        rule.deleteRule(ruleException);
+    }
+    
+    @Test(expected=UnsupportedOperationException.class)
+    public void deleteAllTest(){
+        rule.deleteAll();
+    }
 }
