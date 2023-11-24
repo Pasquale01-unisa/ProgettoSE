a7ddc18ca65cc7b31ddf682653cf9f821e6ea8ed
Elena725 <148887724+Elena725@users.noreply.github.com>
2023-11-24 19:55:39 +0100

    Aggiornamento classe ActionAlarm funzione di esecuzione ringtone

--- ProjectSE/src/projectse/model/action/ActionAlarm.java
+++ ProjectSE/src/projectse/model/action/ActionAlarm.java
@@ -1,38 +1,72 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+ 
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+*/
 package projectse.model.action;
-
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import java.io.File;
 
-/**
- *
- * @author sara
- */
-public class ActionAlarm implements Action{
+public class ActionAlarm implements Action {
     private File file;
+    private MediaPlayer mediaPlayer;
 
     public ActionAlarm(File file) {
         this.file = file;
+        Media media = new Media(file.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
     }
 
+    @Override
+    public void executeAction() {
+        if (mediaPlayer != null) {
+            Platform.runLater(() -> {
+                mediaPlayer.play();
+
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Allarme");
+                alert.setHeaderText("Riproduzione Allarme");
+                alert.setContentText("Premi Interrompi per fermare l'allarme.");
+
+                ButtonType buttonTypeInterrompi = new ButtonType("Interrompi");
+                alert.getButtonTypes().setAll(buttonTypeInterrompi);
+
+                alert.showAndWait().ifPresent(response -> {
+                    if (response == buttonTypeInterrompi) {
+                        mediaPlayer.stop();
+                    }
+                });
+            });
+        } else {
+            System.err.println("MediaPlayer non è stato inizializzato.");
+        }
+    }
+
+    // Metodo per fermare la riproduzione, se necessario
+    public void stopPlaying() {
+        if (mediaPlayer != null && mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING) {
+            mediaPlayer.stop();
+        }
+    }
+
+    // Getter e Setter
     public File getFile() {
         return file;
     }
 
     public void setFile(File file) {
         this.file = file;
+        Media media = new Media(file.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
     }
-
-    @Override
-    public void executeAction() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+    
     @Override
     public String getAction() {
         return "Alarm -> " + file.toString();
     }
-    
+
 }
