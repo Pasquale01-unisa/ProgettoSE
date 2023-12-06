32f728568f74d5f1c1876053e38f95392967999c
SaraAbagnale <134072819+SaraAbagnale@users.noreply.github.com>
2023-12-06 18:18:43 +0100

    Aggiornamento interfaccia, creazione e implementazioni class ActionOpenExternalProgram e ActionOpenExternalProgramFactory

---
+++ ProjectSE/src/projectse/model/action/ActionOpenExternalProgramFactory.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectse.model.action;
+
+import java.io.File;
+
+/**
+ *
+ * @author sara
+ */
+public class ActionOpenExternalProgramFactory extends ActionFactory{
+    private String args;
+    private File file;
+
+    public ActionOpenExternalProgramFactory(String args, File file) {
+        this.args = args;
+        this.file = file;
+    }
+
+    @Override
+    public Action createAction() {
+        return new ActionOpenExternalProgram(args, file);
+    }
+}
